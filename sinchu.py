87d6bcf05b5ac9633917837c671f386823c345e2
9148449392 <sinchanasinchu3020@gmail.com>
2023-08-24 03:44:33 +0530

    second commit

---
+++ sinchu.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+a=[1,2,3,4]
+sum= np.sum(a)
+print("sum of array element is ",sum)
+min= np.min(a)
+print("minimum of array element is ",min)
+max= np.max(a)
+print("maximum of array element is ",max)
+mean= np.mean(a)
+print("mean of array element is ",mean)
+med= np.median(a)
+print("median of array element is ",med)
+cor= np.corrcoef(a)
+print("Correaltion coefficiant of array element is ",cor)
+std= np.std(a)
+print("standard deviation of array element is ",sum)
+
+
+# In[ ]:
+
+
+import numpy as np
+a=[1,2,3,4]
+sum= np.sum(a)
+print("sum of array element is ",sum)
+min= np.min(a)
+print("minimum of array element is ",min)
+max= np.max(a)
+print("maximum of array element is ",max)
+mean= np.mean(a)
+print("mean of array element is ",mean)
+med= np.median(a)
+print("median of array element is ",med)
+cor= np.corrcoef(a)
+print("Correaltion coefficiant of array element is ",cor)
+std= np.std(a)
+print("standard deviation of array element is ",sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import numpy as np
+a=[1,2,3,4]
+sum= np.sum(a)
+print("sum of array element is ",sum)
+min= np.min(a)
+print("minimum of array element is ",min)
+max= np.max(a)
+print("maximum of array element is ",max)
+mean= np.mean(a)
+print("mean of array element is ",mean)
+med= np.median(a)
+print("median of array element is ",med)
+cor= np.corrcoef(a)
+print("Correaltion coefficiant of array element is ",cor)
+std= np.std(a)
+print("standard deviation of array element is ",sum)
+
+
+# In[ ]:
+
+
+
+
